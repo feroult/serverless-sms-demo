2cc1679aff6d22d08bc8a7e55dd3f80e5c9cb506
feroult <feroult@gmail.com>
2017-03-23 17:17:43 -0300

    adding visualizer html and other missing parts

--- cloud-functions/index.js
+++ cloud-functions/index.js
@@ -5,91 +5,93 @@ debug.start();
 var admin = require("firebase-admin");
 
 admin.initializeApp({
-  credential: admin.credential.cert("firebase-service-account.json"),
-  databaseURL: config.firebase.databaseURL
+    credential: admin.credential.cert("firebase-service-account.json"),
+    databaseURL: config.firebase.databaseURL
 });
 var database = admin.database();
 
 var circularJson = require('circular-json');
 var twilio = require('twilio');
 var bigquery = require('@google-cloud/bigquery')({
-	projectId: config.projectId,
-	keyFilename: 'keyfile.json'
+    projectId: config.projectId,
+    keyFilename: 'keyfile.json'
 });
 var language = require('@google-cloud/language')({
-	projectId: config.projectId,
-	keyFilename: 'keyfile.json'
+    projectId: config.projectId,
+    keyFilename: 'keyfile.json'
 });
 
 exports.smsNL = function (req, res) {
-	console.log('smsNL-Visualizer');
-	var fromNumber = req.query.From;
-	var text = req.query.Body;
-	var fromCountry = req.query.FromCountry;
-	var fromCity = req.query.FromCity;
-	let twilioNumber = req.query.To || config.twilio.phoneNumber;
-	let bqTableName = req.query.bq || config.defaultBqTableName;
+    console.log('smsNL-Visualizer');
+    var fromNumber = req.query.From;
+    var text = req.query.Body;
+    var fromCountry = req.query.FromCountry;
+    var fromCity = req.query.FromCity;
+    let twilioNumber = req.query.To || config.twilio.phoneNumber;
+    let bqTableName = req.query.bq || config.defaultBqTableName;
 
-	console.log(`smsNL: "${text}" sent from ${fromNumber}, ${fromCountry},${fromCity}, saving to ${bqTableName}`);
-	language.annotate(text, { verbose: true }, function (err, annotation, apiResponse) {
-		if (err) {
-			console.error(err);
-			res.status(500).send('NL error: ' + err);
-			return;
-		}
-		let emoji = "😄";
-		let sentimentScore = annotation.sentiment.polarity;
-		if (sentimentScore < 50 && sentimentScore > -50) {
-			emoji = "😐";
-		} else if (sentimentScore <= -50) {
-			emoji = "😔";
-		}
-		console.log(`${text} - ${emoji} - ${sentimentScore}`);
+    console.log(`smsNL: "${text}" sent from ${fromNumber}, ${fromCountry},${fromCity}, saving to ${bqTableName}`);
+    language.annotate(text, {verbose: true}, function (err, annotation, apiResponse) {
+        if (err) {
+            console.error(err);
+            res.status(500).send('NL error: ' + err);
+            return;
+        }
+        let emoji = "😄";
+        let sentimentScore = annotation.sentiment.polarity;
+        if (sentimentScore < 50 && sentimentScore > -50) {
+            emoji = "😐";
+        } else if (sentimentScore <= -50) {
+            emoji = "😔";
+        }
+        console.log(`${text} - ${emoji} - ${sentimentScore}`);
 
-		// Write to Firebase.
-		console.log('Writing to Firebase...');
-		database.ref('sms').push({
-			emoji: emoji
-		});
-		
-		// Log to BigQuery.
-		let row = {
-			message_text: text,
-			tokens: JSON.stringify(annotation.tokens),
-			polarity: (annotation.sentiment.polarity).toString(),
-			magnitude: (annotation.sentiment.magnitude).toString(),
-			from_city: fromCity,
-			from_country: fromCountry
-		};
-		let bigQueryDataset = bigquery.dataset(config.bqDatasetName);
-		let bigQueryTable = bigQueryDataset.table(bqTableName);
-		bigQueryTable.insert(row, function (error, insertErr, apiResp) {
-			console.log('Writing to BigQuery...');
-			// console.log(apiResp.insertErrors[0]);
-			if (error) {
-				console.log('err', error);
-				res.status(500).send('BigQuery error');
-				return;
-			} else if (insertErr.length === 0) {
-				let bqStatusMsg = `Written to BQ: ${text} - (${sentimentScore})`;
-				console.log(bqStatusMsg);
-				var client = new twilio.RestClient(config.twilio.accountSid, config.twilio.authToken);
-				// Send a message back.
-				client.messages.create({
-					body: `Based on your message, you seem ${emoji}`,
-					to: fromNumber,  // Text this number
-					from: twilioNumber // From a valid Twilio number
-				}, function (err, message) {
-					if (err) {
-						console.error(err.message);
-						res.status(500).send('Twilio response error.');
-						return;
-					} else {
-						res.status(200).send(bqStatusMsg);
-						return;
-					}
-				});
-			}
-		});
-	});
+        // Write to Firebase.
+        console.log('Writing to Firebase...');
+        database.ref('sms').push({
+            emoji: emoji
+        });
+
+        // Log to BigQuery.
+        let row = {
+            message_text: text,
+            tokens: JSON.stringify(annotation.tokens),
+            polarity: (annotation.sentiment.polarity).toString(),
+            magnitude: (annotation.sentiment.magnitude).toString(),
+            from_city: fromCity,
+            from_country: fromCountry
+        };
+        let bigQueryDataset = bigquery.dataset(config.bqDatasetName);
+        let bigQueryTable = bigQueryDataset.table(bqTableName);
+        bigQueryTable.insert(row, function (error, insertErr, apiResp) {
+            console.log('Writing to BigQuery...');
+            // console.log(apiResp.insertErrors[0]);
+            if (error) {
+                console.log('err', error);
+                res.status(500).send('BigQuery error');
+                return;
+            } else if (insertErr.length === 0) {
+                let bqStatusMsg = `Written to BQ: ${text} - (${sentimentScore})`;
+                console.log(bqStatusMsg);
+                // var client = new twilio.RestClient(config.twilio.accountSid, config.twilio.authToken);
+                // // Send a message back.
+                // client.messages.create({
+                // 	body: `Based on your message, you seem ${emoji}`,
+                // 	to: fromNumber,  // Text this number
+                // 	from: twilioNumber // From a valid Twilio number
+                // }, function (err, message) {
+                // 	if (err) {
+                // 		console.error(err.message);
+                // 		res.status(500).send('Twilio response error.');
+                // 		return;
+                // 	} else {
+                // 		res.status(200).send(bqStatusMsg);
+                // 		return;
+                // 	}
+                // });
+
+                res.status(200).send(text);
+            }
+        });
+    });
 };
--- visualizer/public/index.js
+++ visualizer/public/index.js
@@ -1,13 +1,27 @@
 var db = firebase.database();
 db.ref('sms').on('child_added', snapshot => {
-  console.log('child added');
-  let val = snapshot.val();
-  console.log(val);
-  console.log(val.emoji);
-  if (val.emoji) {
-    let emojiList = window.document.getElementById('emojis');
-    let item = window.document.createElement('span');
-    item.innerText = val.emoji + " ";
-    emojiList.appendChild(item);
-  }
+    console.log('child added');
+    let val = snapshot.val();
+    console.log(val);
+    console.log(val.emoji);
+    if (val.emoji) {
+        let emojiList = window.document.getElementById('emojis');
+        let item = window.document.createElement('span');
+        let d = dimensions();
+        item.innerText = val.emoji + " ";
+        item.style.position = 'absolute';
+        item.style.left = Math.floor(Math.random() * (d.x-100)) + 'px';
+        item.style.top = Math.floor(Math.random() * (d.y-100)) + 'px';
+        emojiList.appendChild(item);
+    }
 });
+
+function dimensions() {
+    var w = window,
+        d = document,
+        e = d.documentElement,
+        g = d.getElementsByTagName('body')[0],
+        x = w.innerWidth || e.clientWidth || g.clientWidth,
+        y = w.innerHeight || e.clientHeight || g.clientHeight;
+    return {x, y};
+}
